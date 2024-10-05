883a7b5f096ea196accb64efa9b5628eb41c721c
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-10-05 08:00:46 -0500

    add(SecondFragment): create SecondFragment with dataBinding

---
+++ app/src/main/java/com/ihuntgore/gradesapp/view/fragments/SecondFragment.kt
@@ -0,0 +1,23 @@
+package com.ihuntgore.gradesapp.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ihuntgore.gradesapp.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSecondBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+}
