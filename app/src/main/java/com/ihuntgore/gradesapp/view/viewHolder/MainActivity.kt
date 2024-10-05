dabc5dcbf06d0f7155a27906d7562e5f602542d6
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-10-05 07:50:37 -0500

    refactor: create similar mvvm architecture

--- app/src/main/java/com/ihuntgore/gradesapp/view/viewHolder/MainActivity.kt
+++ app/src/main/java/com/ihuntgore/gradesapp/view/viewHolder/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.ihuntgore.gradesapp
+package com.ihuntgore.gradesapp.view.viewHolder
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ihuntgore.gradesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
